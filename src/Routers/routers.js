3452485a3a4c3ca670c28f7b443e822e906a897b
Aravinthan <aravinthanpro@gmail.com>
2021-05-27 23:57:46 +0530

    Added the pre condtion for the router

--- src/Routers/routers.js
+++ src/Routers/routers.js
@@ -1,23 +1,48 @@
 const Joi = require("joi");
+const Boom = require('@hapi/boom');
 const siteStatusService = require('../Service/site_status_service');
+
+const preCheck = function (request, h) {
+    console.log("Entered preCheck")
+    if(request.payload.siteUrl.includes("https://")){      
+        const error = Boom.badRequest('Cannot process the Url with https://');
+        throw error;
+    }
+
+    return 'request containig Http url';
+};
+
+
 exports.routes = [
     {
         method: 'POST',
         path: '/siteStatus',
-        config: {
+        // config: {
+        //     validate: {
+        //         payload: Joi.object({
+        //             siteUrl: Joi.string().uri()
+        //         })
+        //     }
+        // },
+        options: {
+            pre: [
+                { method: preCheck, assign: 'm1', failAction : 'error' }
+            ],
             validate: {
                 payload: Joi.object({
                     siteUrl: Joi.string().uri()
                 })
+            },
+            handler: async (request, h) => {
+                console.log("Entered route")
+                // const data = await siteStatusService.checkSiteStatus(request);
+                // const res = h.response();
+                // res.code(200);
+                // res.source = data;
+                // return res ;
+                const data = siteStatusService.checkSiteStatus(request);
+                return data; 
             }
-        },
-        handler: (request, h) => {
-            const data = siteStatusService.checkSiteStatus(request);
-            // return h.response(data);
-            // return siteStatusService.checkSiteStatus(request);
-            // return h.response(siteStatusService.checkSiteStatus(request));
-
-            return data;
         }
     }
 ];
