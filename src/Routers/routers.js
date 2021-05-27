e59c71a8b2215410c14187e967f4ad4793471932
Aravinthan <aravinthanpro@gmail.com>
2021-05-27 14:48:55 +0530

    Added the joi validation

--- src/Routers/routers.js
+++ src/Routers/routers.js
@@ -1,8 +1,16 @@
+const Joi = require("joi");
 const siteStatusService = require('../Service/site_status_service');
 exports.routes = [
     {
         method: 'POST',
         path: '/siteStatus',
+        config: {
+            validate: {
+                payload: Joi.object({
+                    siteUrl: Joi.string().uri()
+                })
+            }
+        },
         handler: (request, h) => {
             return h.response(siteStatusService.checkSiteStatus(request));
         }
